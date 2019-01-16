34155c84c02a1cea81c07aa969e48b5f707a40d4
pentiayu0302 <46737476+pentiayu0302@users.noreply.github.com>
2019-01-16 05:26:13 +0000

    Add files via upload

---
+++ constant.php
@@ -0,0 +1,34 @@
+<?php 
+
+// define('NAMA', 'Penti Ayu');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 17;
+// echo UMUR
+
+// class Coba {
+// 	const NAMA = 'Penti AYU';
+// }
+
+// echo Coba::NAMA;
+
+
+// echo __FILE__;
+
+// function coba() {
+// 	return __FUNCTION__;
+// }
+
+// echo coba();
+
+class Coba {
+	public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+
+
+ ?>
