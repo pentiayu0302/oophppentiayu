6f9dd3037dc4ca3eaae94b2d249bb6c75638913e
pentiayu0302 <46737476+pentiayu0302@users.noreply.github.com>
2019-01-16 05:22:04 +0000

    Add files via upload

---
+++ constructor.php
@@ -0,0 +1,48 @@
+<?php 
+
+class Produk {
+	public $judul,
+		   $penulis,
+		   $penerbit,
+		   $harga;
+
+public function __construct($judul = "judul", $penulis  = "penulis", $penerbit  ="penerbit", $harga  = 0 ){
+	$this->judul = $judul;
+	$this->penulis = $penulis;
+	$this->penerbit = $penerbit;
+	$this->harga = $harga;
+}
+
+
+public function getLabel(){
+	return "$this->penulis, $this->penerbit";
+}
+
+
+
+}
+
+// $produk1 = new Produk();
+// $produk1-> judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2-> judul = "Uncharted";
+// $produk2->tambahProperty = "pentiayu";
+// var_dump($produk2);
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+
+$produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000);
+
+$produk3 = new Produk("Dragon Ball");
+
+
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
+
+ ?>
